7595331a6af41554296c2acaa3835d35b0ba25fc
Bilokur Evgen <bilokur915@gmail.com>
2023-11-21 16:09:11 +0200

    Fixed app routing

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,39 +1,50 @@
 import React, {useContext} from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
-import {adminRoutes, unauthenticatedRoutes, userRoutes} from "../router/routes";
+import {adminRoutes, coachRoutes, unauthenticatedRoutes, userRoutes} from "../router/routes";
 import {ADMIN_ROUTE, MAIN_ROUTE} from "../utils/constants";
 import {AuthContext} from "../context/authContext";
 
 const AppRouter = () => {
-    const {isAuth, roles} = useContext(AuthContext)
+    const {isAuth, roles, isLoading} = useContext(AuthContext)
 
-    if (!isAuth) {
-        return (
-            <Routes>
-                <>
-                    {unauthenticatedRoutes.map(route => <Route
-                        key={route.path} element={route.element} path={route.path}/>)}
-                </>
-                <Route path="*" element={<Navigate to={MAIN_ROUTE}/>}/>
-            </Routes>
-        )
-    } else if (roles.includes('ROLE_ADMIN')) {
-        console.log(roles)
-        console.log("TRUE")
+    if (isLoading) return null;
+
+    if (roles.includes('ROLE_ADMIN')) {
         return (
             <Routes>
                 <>
                     {adminRoutes.map(route => <Route
                         key={route.path} element={route.element} path={route.path}/>)}
+
+                    <Route path="*" element={<Navigate to={ADMIN_ROUTE}/>}/>
                 </>
-                <Route path="*" element={<Navigate to={ADMIN_ROUTE}/>}/>
+            </Routes>
+        )
+    } else if (roles.includes('ROLE_COACH')) {
+        return (
+            <Routes>
+                <>
+                    {coachRoutes.map(route => <Route
+                        key={route.path} element={route.element} path={route.path}/>)}
+                </>
+                <Route path="*" element={<Navigate to={MAIN_ROUTE}/>}/>
+            </Routes>
+        )
+    } else if (roles.includes('ROLE_USER')) {
+        return (
+            <Routes>
+                <>
+                    {userRoutes.map(route => <Route
+                        key={route.path} element={route.element} path={route.path}/>)}
+                </>
+                <Route path="*" element={<Navigate to={MAIN_ROUTE}/>}/>
             </Routes>
         )
     } else {
         return (
             <Routes>
                 <>
-                    {userRoutes.map(route => <Route
+                    {unauthenticatedRoutes.map(route => <Route
                         key={route.path} element={route.element} path={route.path}/>)}
                 </>
                 <Route path="*" element={<Navigate to={MAIN_ROUTE}/>}/>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,7 @@
-import {ADMIN_ROUTE, MAIN_ROUTE, SIGNIN_ROUTE, SIGNUP_ROUTE} from "../utils/constants";
+import {MAIN_ROUTE, SIGNIN_ROUTE, SIGNUP_ROUTE} from "../utils/constants";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import Main from "../pages/Main";
-import Customers from "../pages/Customers";
 
 export const unauthenticatedRoutes = [
     {path : MAIN_ROUTE, element: <Main/>},
@@ -15,7 +14,6 @@ export const userRoutes = [
 ]
 
 export const adminRoutes = [
-    {path: ADMIN_ROUTE, element: <Customers/>},
     {path: MAIN_ROUTE, element: <Main/>},
 ]
 
