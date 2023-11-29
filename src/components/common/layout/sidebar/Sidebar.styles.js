49c5e9508d67693c0f3da90a9e79ccbb634aa177
ivanchikk <vanyaklochkoff@gmail.com>
2023-11-29 20:32:31 +0200

    Implemented sidebar, fixed page layout, refactored routes

---
+++ src/components/common/layout/sidebar/Sidebar.styles.js
@@ -0,0 +1,94 @@
+import {Drawer, styled} from "@mui/material";
+
+const sidebarDrawerWidth = 240;
+
+const openedMixin = (theme) => ({
+    width: sidebarDrawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+})
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+})
+
+export const SidebarDrawerSpacer = styled('div')(
+    ({theme}) => ({
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar and above footer
+        ...theme.mixins.toolbar,
+    }))
+
+export const SidebarDrawer = styled(Drawer, {shouldForwardProp: (prop) => prop !== 'open'})(
+    ({theme, open}) => ({
+        width: sidebarDrawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+)
+
+export const sidebar = {
+    zIndex: 1,
+    display: 'flex',
+}
+
+export const divider = (isSidebarOpened) => ({
+    display: 'flex',
+    alignItems: 'center',
+    minHeight: '32px',
+    transform: !isSidebarOpened ? 'translateY(-50%)' : 'none',
+    borderColor: 'black',
+    "&::before, &::after": {
+        borderColor: "black",
+    },
+})
+
+export const list = {}
+
+export const listItem = {
+    display: 'block',
+}
+
+export const listItemButton = (isSidebarOpened) => ({
+    minHeight: 48,
+    justifyContent: isSidebarOpened ? 'initial' : 'center',
+})
+
+export const listItemButtonCollapse = (isSidebarOpened, theme) => ({
+    ...listItemButton(isSidebarOpened),
+    color: theme.palette.info.main,
+})
+
+export const listItemIcon = (isSidebarOpened) => ({
+    minWidth: 0,
+    marginRight: isSidebarOpened ? 3 : 'auto',
+    justifyContent: 'center',
+})
+
+export const listItemIconCollapse = (isSidebarOpened, theme) => ({
+    ...listItemIcon(isSidebarOpened),
+    color: theme.palette.info.main,
+})
--- src/router/routes.js
+++ src/router/routes.js
@@ -7,7 +7,7 @@ import {
     COACHES_ROUTE,
     FAQ_ROUTE,
     MAIN_ROUTE,
-    PROFILE_ROUTE_BUY_SUBSCRIPTION,
+    PROFILE_ROUTE,
     PROFILE_ROUTE_SUBSCRIPTIONS,
     SIGNIN_ROUTE,
     SIGNUP_ROUTE
@@ -17,13 +17,13 @@ import SignUp from "../pages/sign-up/SignUp";
 import Main from "../pages/main/Main";
 import Faq from "../pages/faq/Faq";
 import Coaches from "../pages/coaches/Coaches";
-import Customers from "../pages/admin/customers/Customers";
-import Records from "../pages/coach/records/Records";
-import Staff from "../pages/admin/staff/Staff";
-import Dashboard from "../pages/admin/dashboard/Dashboard";
+import Profile from "../pages/profile/Profile";
 import MySubscriptions from "../pages/user/mySubscriptions/MySubscriptions";
-import BuySubscription from "../pages/user/buySubscription/BuySubscription";
+import Records from "../pages/coach/records/Records";
+import Customers from "../pages/admin/customers/Customers";
+import Staff from "../pages/admin/staff/Staff";
 import Subscriptions from "../pages/admin/subscriptions/Subscriptions";
+import Dashboard from "../pages/admin/dashboard/Dashboard";
 
 export const defaultRoutes = [
     {path: MAIN_ROUTE, element: <Main/>},
@@ -31,27 +31,33 @@ export const defaultRoutes = [
     {path: COACHES_ROUTE, element: <Coaches/>},
 ]
 
+export const authenticatedRoutes = [
+    {path: PROFILE_ROUTE, element: <Profile/>},
+]
+
 export const unauthenticatedRoutes = [
     ...defaultRoutes,
     {path: SIGNIN_ROUTE, element: <SignIn/>},
-    {path: SIGNUP_ROUTE, element: <SignUp/>}
+    {path: SIGNUP_ROUTE, element: <SignUp/>},
 ]
 
 export const userRoutes = [
+    ...authenticatedRoutes,
     ...defaultRoutes,
     {path: PROFILE_ROUTE_SUBSCRIPTIONS, element: <MySubscriptions/>},
-    {path: PROFILE_ROUTE_BUY_SUBSCRIPTION, element: <BuySubscription/>},
 ]
 
 export const adminRoutes = [
+    ...authenticatedRoutes,
     ...defaultRoutes,
     {path: ADMIN_ROUTE_CUSTOMERS, element: <Customers/>},
-    {path: ADMIN_ROUTE_SUBSCRIPTIONS, element: <Subscriptions/>},
-    {path: ADMIN_ROUTE_STAFF, element: <Staff/>},
     {path: ADMIN_ROUTE_DASHBOARD, element: <Dashboard/>},
+    {path: ADMIN_ROUTE_STAFF, element: <Staff/>},
+    {path: ADMIN_ROUTE_SUBSCRIPTIONS, element: <Subscriptions/>},
 ]
 
 export const coachRoutes = [
+    ...authenticatedRoutes,
     ...defaultRoutes,
     {path: COACH_ROUTE_RECORDS, element: <Records/>},
 ]
