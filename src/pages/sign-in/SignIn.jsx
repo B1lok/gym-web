0c6cc2285619b7c33560710f2b5ed8725cc3af82
ivanchikk <vanyaklochkoff@gmail.com>
2023-12-01 06:49:09 +0200

    Refactored sign in and sign out, added sign in util

--- src/pages/sign-in/SignIn.jsx
+++ src/pages/sign-in/SignIn.jsx
@@ -1,18 +1,19 @@
 import React, {useContext} from 'react';
+import * as styles from './SignIn.styles';
 import {useNavigate} from "react-router-dom";
-import {MAIN_ROUTE, SIGNUP_ROUTE} from "../../utils/constants";
+import {SIGNUP_ROUTE} from "../../utils/constants";
 import {AuthContext} from "../../context/authContext";
 import AuthService from "../../api/AuthService";
-import {jwtDecode} from "jwt-decode";
 import {Alert, Box, Button, Container, CssBaseline, Grid, Link, TextField, Typography} from '@mui/material';
-import {useForm} from "react-hook-form";
+import {Controller, useForm} from "react-hook-form";
+import {signIn} from "../../utils/utils";
 
 const SignIn = () => {
     const navigate = useNavigate()
     const {setIsAuth, setToken, setRoles} = useContext(AuthContext)
 
     const {
-        register,
+        control,
         handleSubmit,
         formState: {errors},
         setError,
@@ -30,53 +31,51 @@ const SignIn = () => {
             return
         }
 
-        setIsAuth(true)
-        localStorage.setItem('auth', 'true')
-        setToken(response.data.token)
-        localStorage.setItem('token', response.data.token)
-
-        const decodedJwt = jwtDecode(response.data.token)
-        setRoles(decodedJwt.roles)
-        localStorage.setItem('roles', JSON.stringify(decodedJwt.roles))
-        navigate(MAIN_ROUTE)
+        signIn(navigate, setIsAuth, setToken, setRoles, response.data.token)
     }
 
     return (
         <Container component="main" maxWidth="xs">
             <CssBaseline/>
-            <Box
-                sx={{
-                    display: 'flex',
-                    minHeight: '100vh',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}
-            >
-                <Typography component="h1" variant="h5">
+            <Box sx={styles.main}>
+                <Typography component="h1" variant="h4">
                     Sign in
                 </Typography>
-                <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{mt: 2}}>
+                <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate mt={2}>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
-                            <TextField
-                                {...register('email')}
-                                id="email"
-                                label="Email"
-                                autoComplete="email"
-                                required
-                                fullWidth
+                            <Controller
+                                name="email"
+                                control={control}
+                                defaultValue=""
+                                render={({field}) => (
+                                    <TextField
+                                        {...field}
+                                        id="email"
+                                        label="Email"
+                                        autoComplete="email"
+                                        required
+                                        fullWidth
+                                    />
+                                )}
                             />
                         </Grid>
                         <Grid item xs={12}>
-                            <TextField
-                                {...register('password')}
-                                id="password"
-                                label="Password"
-                                type="password"
-                                autoComplete="current-password"
-                                required
-                                fullWidth
+                            <Controller
+                                name="password"
+                                control={control}
+                                defaultValue=""
+                                render={({field}) => (
+                                    <TextField
+                                        {...field}
+                                        id="password"
+                                        label="Password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        required
+                                        fullWidth
+                                    />
+                                )}
                             />
                         </Grid>
                     </Grid>
@@ -86,12 +85,13 @@ const SignIn = () => {
                     <Grid container sx={{mt: 1, mb: 3}}>
                         <Grid item>
                             <Link href={SIGNUP_ROUTE} variant="body2">
-                                {"Don't have an account? Sign Up"}
+                                Don't have an account? Sign Up
                             </Link>
                         </Grid>
                     </Grid>
                     {errors.root?.serverError &&
-                        <Alert severity={"error"}>{errors.root.serverError?.type ?? 'Unknown error'}</Alert>}
+                        <Alert severity={"error"}>{errors.root.serverError?.type ?? 'Unknown error'}</Alert>
+                    }
                 </Box>
             </Box>
         </Container>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,28 @@
 import {MAIN_ROUTE} from "./constants";
+import {jwtDecode} from "jwt-decode";
+
+export const signIn = (navigate, setIsAuth, setToken, setRoles, token) => {
+    const decodedJwt = jwtDecode(token)
+
+    setIsAuth(true)
+    setToken(token)
+    setRoles(decodedJwt.roles)
+
+    localStorage.setItem('auth', 'true')
+    localStorage.setItem('token', token)
+    localStorage.setItem('roles', JSON.stringify(decodedJwt.roles))
+
+    navigate(MAIN_ROUTE)
+}
 
 export const signOut = (navigate, setIsAuth, setToken, setRoles) => {
     setIsAuth(false)
     setToken('')
     setRoles([])
+
     localStorage.removeItem('auth')
     localStorage.removeItem('roles')
     localStorage.removeItem('token')
+
     navigate(MAIN_ROUTE)
 }
